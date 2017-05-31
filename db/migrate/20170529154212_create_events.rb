e35f086b42b7881ebea47d58c23a61ff7eafd59b
eduardo-salazar <esalazar922@gmail.com>
2017-05-31 23:52:07 +0800

    Added all tables needed

--- db/migrate/20170529154212_create_events.rb
+++ db/migrate/20170529154212_create_events.rb
@@ -1,11 +1,25 @@
 class CreateEvents < ActiveRecord::Migration[5.1]
   def change
     create_table :events do |t|
-      t.string :id
+      t.string :event_id
       t.string :name
       t.string :location
+      t.string :host
       t.date :date
       t.string :img_path
+    end
+
+    create_table :event_participants do |t|
+      t.string :event_id
+      t.string :user_id
+      t.string :role_id
+    end
+
+    create_table :event_menus do |t|
+      t.string :event_id
+      t.string :name
+      t.string :type_id
+      t.string :recipe
     end
   end
 end
