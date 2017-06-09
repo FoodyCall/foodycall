b87dd79e785b16ddd5436790ce7ef92c80095a72
eduardo-salazar <esalazar922@gmail.com>
2017-06-09 21:09:36 +0800

    Added initial structure of event json

--- models/event.rb
+++ models/event.rb
@@ -3,4 +3,20 @@ class Event < Sequel::Model
   plugin :timestamps, update_on_create: true
   many_to_one :user, :key=>:host_id
   one_to_many :event_participants
+
+  def to_json(options = {})
+    JSON({
+        name: name,
+        location: location,
+        date: date,
+        host:{},
+        chef:
+        helper:
+        shopper:
+        cleaner:
+        guest:
+        participants: []
+        menu:[]
+      },options)
+  end
 end
