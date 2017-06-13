455e7ec97ebf3fcffd240b8beb6c6ae3e1b16471
Luis Robles <roblescoulter@gmail.com>
2017-06-13 11:21:26 +0800

    added design to Log in and Signup buttons

---
+++ config/conf_env.rb
@@ -0,0 +1,11 @@
+
+config_env :development, :test do
+  set 'GZmU3MCsOFQnhdxeRKJBZhwlay9Kd5LSYtVJl3T297Y=',''
+end
+
+config_env :test do
+end
+
+config_env :production do
+  set 'GZmU3MCsOFQnhdxeRKJBZhwlay9Kd5LSYtVJl3T297Y=',''
+end
